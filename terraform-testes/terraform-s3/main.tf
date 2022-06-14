11d3b6322d8a7b7cad1a430de981969a17a44a73
Luiz Felipe Vera Cruz <luizfelipeveracruz2010@gmail.com>
2022-06-14 19:38:23 -0300

    primeiro deploy s3 e começo ec2

---
+++ terraform-testes/terraform-s3/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "bucketterraform-ferruge"
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
